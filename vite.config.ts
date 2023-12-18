e6bdadc6d316996063aeed2b513d110621863052
ipopov <igor.popov.develop@gmail.com>
2023-12-18 23:11:52 +0300

    add ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
-import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [handlebars()],
   server: {
     port: 3000,
   },
+  build: {
+    outDir: 'dist'
+}
 }) 
